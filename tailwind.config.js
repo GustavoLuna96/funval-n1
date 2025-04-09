a5df1ae07d1d011077f78beefee236cb14df8969
Gustavo <tavoooo92@hotmail.com>
2025-04-08 18:51:36 -0600

    last exercise

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 export default {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+
+        colors:{
+          'light-slate-blue': 'hsl(252, 100%, 67%)',
+        }
+      },
     },
     plugins: [],
   };
